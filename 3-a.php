e1e0c626dd9247b6e5a9cdec517b47e6291b10f2
Ida Ernawati <125109186+idaaernawati@users.noreply.github.com>
2023-04-18 12:31:26 +0700

    Add files via upload

---
+++ 3-a.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "myDB";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "CREATE TABLE liga (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    kode CHAR(3) NOT NULL,
+    negara VARCHAR(30) NULL,
+    champion int(3))";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
